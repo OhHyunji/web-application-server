346522c5a0e3ec6b95641950f6300887c752fcd8
azalea <azalea.oh@kakaocorp.com>
2020-03-28 01:48:21 +0900

    3.4.3.1 요구사항1 - index.html 응답하기(p.96)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,25 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
+
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final byte[] DEFAULT_RESPONSE_BODY = "Hello World".getBytes();
 
     private Socket connection;
 
@@ -19,18 +28,52 @@ public class RequestHandler extends Thread {
     }
 
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String path = getResourcePath(in);
+            byte[] body = Optional.ofNullable(path).map(this::getBody).orElse(DEFAULT_RESPONSE_BODY);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String getResourcePath(InputStream in) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+            while (true) {
+                String str = bufferedReader.readLine();
+
+                if(Strings.isNullOrEmpty(str)) {
+                    break;
+                }
+
+                if(str.contains("GET")) {
+                    String[] requestInfo = str.split(" ");
+                    // TODO length>2 다른사람들은 어떻게 처리했는지?
+                    return requestInfo[1];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private byte[] getBody(String path) {
+        try {
+            return Files.readAllBytes(Paths.get("./webapp" + path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return DEFAULT_RESPONSE_BODY;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
